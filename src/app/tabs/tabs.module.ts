b0da548873a2647dda0cfad7fd2bae88510d783a
rob-ochoa <39144264+rob-ochoa@users.noreply.github.com>
2019-02-17 17:17:05 -0500

    [Add]: Navigation Tabs
    Se agregan tabs de navegacion: Menu, Home y Peticiones

---
+++ src/app/tabs/tabs.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: '../home/home.module#HomePageModule'
+      },
+      {
+        path: 'menu',
+        loadChildren: '../menu/menu.module#MenuPageModule'
+      },
+      {
+        path: 'petition',
+        loadChildren: '../petition/petition.module#PetitionPageModule'
+      },
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [TabsPage]
+})
+export class TabsPageModule {}
