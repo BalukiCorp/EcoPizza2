c41e757a6c12407464bbb1ace16e5df208abfd01
rob-ochoa <39144264+rob-ochoa@users.noreply.github.com>
2019-02-17 18:47:49 -0500

    [Add]: Navigation On Card
    Navegacion al darl click en Card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
 //  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  {path: 'petition', loadChildren: './petition/petition.module#PetitionPageModule'},
 ];
 
 @NgModule({
