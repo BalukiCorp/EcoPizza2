c76ac38541f8a86bbc3d2c6838f179d2c7f9524c
DeathScript <robochoa@est.ecotec.edu.ec>
2019-02-20 19:04:16 -0500

    [Update]: Routing Module
    Navegacion a pagina petition habilitado.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  //{path: 'petition', loadChildren: './petition/petition.module#PetitionPageModule'},
+  {path: 'petition', loadChildren: './petition/petition.module#PetitionPageModule'},
   { path: 'login', loadChildren: './login/login.module#LoginPageModule'},
   { path: 'user', loadChildren: './user/user.module#UserPageModule' },
 ];
